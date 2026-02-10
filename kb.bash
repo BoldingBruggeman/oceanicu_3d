2b2c9b897b497c6df18c989b2ae4ae3e545eb297
Karsten Bolding <karsten@bolding-bruggeman.com>
2026-02-10 16:42:51 +0100

    don't activate pygetm explicitly

--- kb.bash
+++ kb.bash
@@ -7,16 +7,14 @@ if [ "$(hostname)" == "orca" ]; then
   export HYDROGRAPHY_FOLDER="/server/data/WOA"
   export TPXO_FOLDER="/server/data/TPXO9"
   export METEO_FOLDER="/data/ERA5/NA"
-  export RIVER_FOLDER="/data/emorid"
+  export RIVER_FOLDER="/data/EMORID"
   export RIVER_FILE="EMORID_1990_2024.nc"
   export FABM_FOLDER="/data/FABM"
 
 fi
 
-export SIMULATION_INITIAL_DATE="2020-01-01"
-export SIMULATION_START_DATE="2020-01-01"
+export SIMULATION_INITIAL_DATE="2020-12-01"
+export SIMULATION_START_DATE="2020-12-01"
 export SIMULATION_STOP_DATE="2022-01-01"
 
-conda activate pygetm
-python run_chunks.py run_model.py ns_config.yaml "/data/$USER/NS" "$NP" --exp test
-conda deactivate
+conda run -n pygetm --no-capture-output python run_chunks.py run_model.py ns_config.yaml "/data/$USER/NS" 6 --exp test
